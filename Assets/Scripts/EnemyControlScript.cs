09b26431e91f6ff0f2c4d8225eaa470a47bbc74c
mwallis <mattwallis@tactica.ca>
2023-09-24 13:17:23 -0500

    - Improvements to tracking calculations - Smoothing to animation via exit time in blend tree - Removal of backwards movement blend tree - Correction to velocity decomposition calculation with rads conversion

--- Assets/Scripts/EnemyControlScript.cs
+++ Assets/Scripts/EnemyControlScript.cs
@@ -7,26 +7,17 @@ public class EnemyControlScript : MonoBehaviour
 {
 
     public GameObject player;
-
     public GameObject gameBall;
-
     public GameObject homeSquare;
-	
-	public float animSpeed;
-
+    public float animSpeed;
     public float linAccelMax;
-
     public float linVelMax;
 
     private Animator anim;
-
     private Rigidbody rbody;
     private Rigidbody ballRbody;
-    // private Rigidbody playerRbody;
-
     private Vector3 targetLocation;
     private Quaternion targetRotation;
-    
     private float minimumX;
     private float maximumX;
     private float minimumZ;
@@ -34,7 +25,6 @@ public class EnemyControlScript : MonoBehaviour
 	private float centerX;
 	private float centerZ;
     private Collider homeSquareCollider;
-
     private float linVelCurrent;
 
     // Start is called before the first frame update
@@ -44,11 +34,7 @@ public class EnemyControlScript : MonoBehaviour
         anim = GetComponent<Animator>();
         rbody = GetComponent<Rigidbody>();
         ballRbody = gameBall.GetComponent<Rigidbody>();
-        // playerRbody = player.GetComponent<Rigidbody>();
         homeSquareCollider = homeSquare.GetComponent<Collider>();
-        
-        centerX = homeSquareCollider.bounds.center.x;
-		centerZ = homeSquareCollider.bounds.center.z;
         linVelCurrent = 0;
         CalculateTarget();
         DeterminePlayerBounds();
@@ -67,7 +53,7 @@ public class EnemyControlScript : MonoBehaviour
 
     void FixedUpdate()
     {
-        double linearTolerance = 0.2;
+        double linearTolerance = 1;
         double angleTolerance = 3.0;
         Quaternion newRotation;
         float newAcceleration = linAccelMax;
@@ -85,7 +71,7 @@ public class EnemyControlScript : MonoBehaviour
         {
             anim.SetBool("IsTraversing", true);
 
-            float differenceInAngles = Mathf.Abs(this.transform.eulerAngles.y - targetRotation.eulerAngles.y);
+            float differenceInAngles = Vector3.Angle(this.transform.forward, targetLocation - this.transform.position);
             if (differenceInAngles > linearTolerance || 1 == 1)
             {
                 newRotation = Quaternion.LerpUnclamped(this.transform.rotation, targetRotation, 1.0f);
@@ -112,13 +98,24 @@ public class EnemyControlScript : MonoBehaviour
                 );
 
             linVelCurrent = newVelocity;
-            velx = linVelCurrent / linVelMax * Mathf.Cos(90 - differenceInAngles);
-            vely = linVelCurrent / linVelMax * Mathf.Cos(differenceInAngles);
+            velx = linVelCurrent / linVelMax * Mathf.Sin(differenceInAngles * Mathf.Deg2Rad);
+            vely = linVelCurrent / linVelMax * Mathf.Cos(differenceInAngles * Mathf.Deg2Rad);
             
         }
         else
         {
             anim.SetBool("IsTraversing", false);
+    
+            Vector3 toTarget = (targetLocation - this.transform.position).normalized;
+            float turnAngle = Vector3.SignedAngle(this.transform.forward, toTarget, Vector3.up);
+            if (Mathf.Abs(turnAngle) > angleTolerance)
+            {
+                velx = Mathf.Sign(turnAngle) * 1.0f; 
+            }
+            else
+            {
+                velx = 0;
+            }
         }
         
         anim.SetFloat("velx", velx);
@@ -155,7 +152,7 @@ public class EnemyControlScript : MonoBehaviour
 
     void OnTriggerExit(Collider other)
     {
-        if (other.CompareTag("Ball"))
+        if (other.CompareTag("Ball") && BallNotInQuadrant())
         {
             anim.SetBool("TargetPlayer", true);
             anim.SetBool("TargetBall", false);
@@ -166,19 +163,23 @@ public class EnemyControlScript : MonoBehaviour
         }
     }
 
+    private bool BallNotInQuadrant()
+    {
+        return true; //placeholder
+    }
+
     private Vector3 PredictBall()
     {
         Vector3 distanceFromTarget = gameBall.transform.position - rbody.transform.position;
         float magFromTarget = Mathf.Sqrt(Mathf.Pow(distanceFromTarget.z, 2.0f) + Mathf.Pow(distanceFromTarget.x, 2.0f));
-        //float lookAheadTime = distanceFromTarget.magnitude / animSpeed;
         float lookAheadTime = magFromTarget / linVelMax;
-        Vector3 ballXZVel = ballRbody.velocity;
-        ballXZVel.y = 0;
-       // Vector3 futureTarget = gameBall.transform.position + (lookAheadTime * ballRbody.velocity);
-        Vector3 futureTarget = gameBall.transform.position + (lookAheadTime * ballXZVel);
-        futureTarget.y = 0;
+        Vector3 ballPos = gameBall.transform.position;
+        Vector3 ballVel = ballRbody.velocity;
+        float futureX = ballPos.x + ballVel.x * lookAheadTime;
+        float futureY = ballPos.y + ballVel.y * lookAheadTime + 0.5f * Physics.gravity.y * Mathf.Pow(lookAheadTime, 2.0f);
+        float futureZ = ballPos.z + ballVel.z * lookAheadTime;
 
-        return futureTarget;
+        return new Vector3(futureX, futureY, futureZ);
     }
 
     private void CalculateTarget()
@@ -209,6 +210,9 @@ public class EnemyControlScript : MonoBehaviour
 
     private void DeterminePlayerBounds()
     {
+        centerX = homeSquareCollider.bounds.center.x;
+        centerZ = homeSquareCollider.bounds.center.z;
+        
         if (homeSquare.CompareTag("Square1"))
         {
             minimumX = (-1) * Mathf.Infinity;
@@ -256,6 +260,20 @@ public class EnemyControlScript : MonoBehaviour
     
     void OnDrawGizmos()
     {
+        Gizmos.color = Color.green;
+        float lineLength = 2.0f;
+        Vector3 forwardPosition = transform.position + transform.forward * lineLength;
+    
+        Gizmos.DrawLine(transform.position, forwardPosition);
+        
+        float arrowHeadAngle = 25.0f; 
+        float arrowHeadLength = 0.5f; 
+
+        Vector3 rightArrow = Quaternion.LookRotation(transform.forward) * Quaternion.Euler(0,180+arrowHeadAngle,0) * new Vector3(0,0,1);
+        Vector3 leftArrow = Quaternion.LookRotation(transform.forward) * Quaternion.Euler(0,180-arrowHeadAngle,0) * new Vector3(0,0,1);
+
+        Gizmos.DrawRay(forwardPosition, rightArrow * arrowHeadLength);
+        Gizmos.DrawRay(forwardPosition, leftArrow * arrowHeadLength);
         if (GetComponent<CapsuleCollider>() != null)
         {
             Vector3 size = new Vector3(1f, 1f, 1f);

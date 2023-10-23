cafabbfa1aa7e025ce40f1baf87fb0af3ff1868a
aduan3 <rebcap-fuzBib-9kisto>
2023-10-22 20:32:58 -0400

    Added NavMeshAgent

---
+++ Assets/AIPlayerController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using DefaultNamespace;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class AIPlayerController : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    
+    public AIPlayerState aiState = AIPlayerState.CatchBall;
+    public GameObject homeSquare;
+    public GameObject ball;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        if (agent == null)
+            Debug.Log("NavMeshAgent could not be found");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        BallThrowing bt = ball.GetComponent<BallThrowing>();
+        
+        // ball is hitting to our location
+        if (homeSquare.CompareTag(bt.targetSquareTag))
+        {
+            agent.SetDestination(bt.targetLocation);    
+        }
+    }
+}

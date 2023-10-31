d2b86a47276746e53e30df53be8cdeb71c04c7b4
aduan3 <rebcap-fuzBib-9kisto>
2023-10-30 21:15:11 -0400

    Code clean up. remove not effective code

--- Assets/Scripts/AIPlayerController.cs
+++ Assets/Scripts/AIPlayerController.cs
@@ -19,43 +19,19 @@ public class AIPlayerController : MonoBehaviour
     private Rigidbody rbody;
     private Rigidbody ballRbody;
     private Vector3 targetLocation;
-    private Quaternion targetRotation;
-    private float minimumX;
-    private float maximumX;
-    private float minimumZ;
-    private float maximumZ;
-    private float centerX;
-    private float centerZ;
-    private Collider homeSquareCollider;
-    private SquareLocation homeSquareEnum;
-    private float linVelCurrent;
-    
+
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         if (agent == null)
             Debug.Log("NavMeshAgent could not be found");
-        DeterminePlayerBounds();
     }
 
     // Update is called once per frame
     void Update()
     {
         BallThrowing bt = ball.GetComponent<BallThrowing>();
-        Vector3 targetLocation;
-
-        // If ball is moving with player-applied force -- this will 
-        // be pertinent when we add ball prediction back, for now behaviour is identical in each
-        // case.
-        if (bt._freeTargeting)
-        {
-            targetLocation = CalculateTargetInBounds(bt.targetLocation);
-        }
-        else
-        {
-            targetLocation = bt.targetLocation;
-        }
         
         // ball is hitting to our location
         if (bt._targetSquare != null && homeSquare.CompareTag(bt._targetSquare.tag))
@@ -86,55 +62,6 @@ public class AIPlayerController : MonoBehaviour
         }
     }
     
-    private Vector3 CalculateTargetInBounds(Vector3 target)
-    {
-        Vector3 clampedTarget;
-        clampedTarget.x = Mathf.Clamp(target.x, minimumX, maximumX);
-        clampedTarget.z = Mathf.Clamp(target.z, minimumZ, maximumZ);
-        clampedTarget.y = target.y;
-
-        return clampedTarget;
-    }
-    
-    private void DeterminePlayerBounds()
-    {
-        
-        Collider homeSquareCollider = homeSquare.GetComponent<Collider>();
-        centerX = homeSquareCollider.bounds.center.x;
-        centerZ = homeSquareCollider.bounds.center.z;
-
-        if (homeSquare.CompareTag("square_one"))
-        {
-            minimumX = (-1) * Mathf.Infinity;
-            minimumZ = (-1) * Mathf.Infinity;
-            maximumX = homeSquareCollider.bounds.max.x;
-            maximumZ = homeSquareCollider.bounds.max.z;
-
-        }
-        else if (homeSquare.CompareTag("square_two"))
-        {
-            minimumX = (-1) * Mathf.Infinity;
-            minimumZ = homeSquareCollider.bounds.min.z;
-            maximumX = homeSquareCollider.bounds.max.x;
-            maximumZ = Mathf.Infinity;
-
-        }
-        else if (homeSquare.CompareTag("square_three"))
-        {
-            minimumX = homeSquareCollider.bounds.min.x;
-            minimumZ = homeSquareCollider.bounds.min.z;
-            maximumX = Mathf.Infinity;
-            maximumZ = Mathf.Infinity;
-        }
-        else if (homeSquare.CompareTag("square_four"))
-        {
-            minimumX = homeSquareCollider.bounds.min.x;
-            minimumZ = (-1) * Mathf.Infinity;
-            maximumX = Mathf.Infinity;
-            maximumZ = homeSquareCollider.bounds.max.z;
-        }
-    }
-
     private void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.CompareTag("Ball"))

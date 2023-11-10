4b7e0860114a40325aac49de77565beedcc7e89e
aduan3 <rebcap-fuzBib-9kisto>
2023-11-09 19:06:10 -0500

    Fixed pause menu canvas z orders with count down screen

--- Assets/CountDownController.cs
+++ Assets/CountDownController.cs
@@ -3,22 +3,26 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 using UnityEngine.UI;
 
 public class CountDownController : MonoBehaviour
 {
     public int countDownTime;
     public TextMeshProUGUI countDownText;
-
+    public EventSystem eventSystem;
     private void Awake()
     {
+        Time.timeScale = 1;
         StartCoroutine(CountdownToStart());
     }
 
     IEnumerator CountdownToStart()
     {
         CanvasGroup canvasGroup = GetComponent<CanvasGroup>();
-        
+
+        eventSystem.sendNavigationEvents = false;
         canvasGroup.interactable = true;
         canvasGroup.blocksRaycasts = true;
         canvasGroup.alpha = 1;
@@ -37,5 +41,6 @@ public class CountDownController : MonoBehaviour
         canvasGroup.interactable = false;
         canvasGroup.blocksRaycasts = false;
         canvasGroup.alpha = 0;
+        eventSystem.sendNavigationEvents = true;
     }
 }
